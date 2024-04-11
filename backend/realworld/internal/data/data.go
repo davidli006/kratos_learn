13b7ce12911c4e20f372e7695f012d846a48054e
李栋 <lidong65550@offcn.com>
2024-04-11 17:01:13 +0800

    data

--- backend/realworld/internal/data/data.go
+++ backend/realworld/internal/data/data.go
@@ -1,6 +1,9 @@
 package data
 
 import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"realworld/internal/conf"
 
 	"github.com/go-kratos/kratos/v2/log"
@@ -8,17 +11,32 @@ import (
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewDB, NewGreeterRepo)
 
 // Data .
 type Data struct {
 	// TODO wrapped database client
+	db *gorm.DB
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+func NewData(c *conf.Data, db *gorm.DB, logger log.Logger) (*Data, func(), error) {
+	db = NewDB(c)
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	return &Data{db: db}, cleanup, nil
+}
+
+func NewDB(c *conf.Data) *gorm.DB {
+	db, err := gorm.Open(mysql.Open(c.Database.Driver), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect database!")
+		return nil
+	}
+	if err := db.AutoMigrate(); err != nil {
+		fmt.Println("failed to migrate!")
+		return nil
+	}
+	return db
 }
